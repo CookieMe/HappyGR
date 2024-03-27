01f419b025e86f321cbba93c1fc168ee4112e820
CookieMe <137884746@qq.com>
2024-03-27 14:12:55 +0800

    Update happygr.js

--- happygr.js
+++ happygr.js
@@ -21,68 +21,40 @@ function getSubscriptionInfo() {
         "request_date": requestDate.toISOString(),
         "request_date_ms": requestDate.getTime(),
         "subscriber": {
-            "entitlements": {
-                "grow.pro": {
-                    "expires_date": null,
-                    "grace_period_expires_date": null,
-                    "product_identifier": "grow_lifetime",
-                    "purchase_date": "2024-03-14T14:44:52Z"
-                }
-            },
-            "first_seen": "2024-03-27T04:26:44Z",
-		    "last_seen": "2024-03-27T04:26:44Z",
-            "management_url": "https://apps.apple.com/account/subscriptions",
-            "non_subscriptions": {
-                "grow_lifetime": [{
-                    "id": "2f58ca11d3",
-                    "is_sandbox": false,
-                    "original_purchase_date": "2024-03-14T14:44:52Z",
-                    "purchase_date": "2024-03-14T14:44:52Z",
-                    "store": "app_store",
-                    "store_transaction_id": "1090000007756035"
-                }]
-            },
-            "original_app_user_id": "$RCAnonymousID:f05892111c2643dcbc3ebecba3878c22",
-            "original_application_version": "270",
-            "original_purchase_date": "2023-11-11T13:31:47Z",
-            "other_purchases": {
-                "grow_lifetime": {
-                    "purchase_date": "2024-03-14T14:44:52Z"
-                }
-            },
-            "subscriptions": {
-                "grow_1y_128": {
-                    "auto_resume_date": null,
-                    "billing_issues_detected_at": null,
-                    "expires_date": "2023-11-18T13:37:58Z",
-                    "grace_period_expires_date": null,
-                    "is_sandbox": false,
-                    "original_purchase_date": "2023-11-11T13:37:59Z",
-                    "ownership_type": "PURCHASED",
-                    "period_type": "trial",
-                    "purchase_date": "2023-11-11T13:37:58Z",
-                    "refunded_at": null,
-                    "store": "app_store",
-                    "store_transaction_id": "570001360500852",
-                    "unsubscribe_detected_at": "2023-11-12T16:15:30Z"
-                },
-                "grow_299_1m": {
-                    "auto_resume_date": null,
-                    "billing_issues_detected_at": null,
-                    "expires_date": "2024-04-06T09:32:46Z",
-                    "grace_period_expires_date": null,
-                    "is_sandbox": false,
-                    "original_purchase_date": "2024-03-03T10:32:47Z",
-                    "ownership_type": "PURCHASED",
-                    "period_type": "normal",
-                    "purchase_date": "2024-03-06T10:32:46Z",
-                    "refunded_at": null,
-                    "store": "app_store",
-                    "store_transaction_id": "1710000004088335",
-                    "unsubscribe_detected_at": "2024-03-15T05:10:33Z"
-                }
-            }
-        }
+		"entitlements": {
+			"grow.pro": {
+				"expires_date": "2025-03-27T06:05:44Z",
+				"grace_period_expires_date": null,
+				"product_identifier": "grow_1y_128",
+				"purchase_date": "2024-03-27T06:05:44Z"
+			}
+		},
+		"first_seen": "2024-03-27T06:01:40Z",
+		"last_seen": "2024-03-27T06:01:40Z",
+		"management_url": "https://apps.apple.com/account/subscriptions",
+		"non_subscriptions": {},
+		"original_app_user_id": "$RCAnonymousID:df1e29fd84e94d0f93aff3632cbb613d",
+		"original_application_version": "270",
+		"original_purchase_date": "2023-10-27T10:42:47Z",
+		"other_purchases": {},
+		"subscriptions": {
+			"grow_1y_128": {
+				"auto_resume_date": null,
+				"billing_issues_detected_at": null,
+				"expires_date": "2025-04-03T06:05:44Z",
+				"grace_period_expires_date": null,
+				"is_sandbox": false,
+				"original_purchase_date": "2024-03-27T06:05:45Z",
+				"ownership_type": "PURCHASED",
+				"period_type": "trial",
+				"purchase_date": "2024-03-27T06:05:44Z",
+				"refunded_at": null,
+				"store": "app_store",
+				"store_transaction_id": "500001675549519",
+				"unsubscribe_detected_at": "2024-03-27T06:09:07Z"
+			}
+		}
+	}
     };
     var body = JSON.stringify(subscriber);
     return body;
