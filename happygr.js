0c1e16585bd7e65e10ddaeb6c2b5829a7b03858c
CookieMe <137884746@qq.com>
2024-03-27 23:28:32 +0800

    Update happygr.js

--- happygr.js
+++ happygr.js
@@ -14,62 +14,75 @@
 [mitm]
 hostname = api.revenuecat.com
  ******************************************/
-/******************************************
- * @Name Grow
- * @Description Unlock Permanent VIP
- * @Channel https://t.me/
- * @Feedback https://t.me/
- * @Author CookieMe
- * @Update 20240326
- * @App Link http://t.cn/A6ILzuhD
- * @App Version laste [Highest Supported]
- ******************************************
-[rewrite_local]
-# > Grow Unlock Permanent VIP
-^https?:\/\/api\.revenuecat\.com\/v1\/(receipts|subscribers\/\$RCAnonymousID%\w{34})$ url script-response-body https://raw.githubusercontent.com/CookieMe/HappyGR/main/happygr.js
-[mitm]
-hostname = api.revenuecat.com
- ******************************************/
 var body = $response.body;
 var obj = JSON.parse(body);
 
 obj = {
-    "request_date_ms": 1711550283261,
-    "request_date": "2024-03-27T14:38:03Z",
+    "request_date": "2024-03-26T16:16:35Z",
+    "request_date_ms": 1711469795035,
     "subscriber": {
-        "non_subscriptions": {},
-        "first_seen": "2024-03-27T06:01:40Z",
-        "original_application_version": "270",
-        "other_purchases": {},
-        "management_url": "https://apps.apple.com/account/subscriptions",
-        "subscriptions": {
-            "grow_1y_128": {
-                "original_purchase_date": "2024-03-27T06:05:45Z",
-                "expires_date": "2034-04-01T06:05:44Z",
-                "is_sandbox": false,
-                "refunded_at": null,
-                "store_transaction_id": "500001675549519",
-                "unsubscribe_detected_at": "2024-03-27T06:09:07Z",
-                "grace_period_expires_date": null,
-                "period_type": "normal",
-                "purchase_date": "2024-03-27T06:05:44Z",
-                "billing_issues_detected_at": null,
-                "ownership_type": "PURCHASED",
-                "store": "app_store",
-                "auto_resume_date": null
-            }
-        },
         "entitlements": {
             "grow.pro": {
+                "expires_date": null,
                 "grace_period_expires_date": null,
-                "purchase_date": "2024-03-27T06:05:44Z",
-                "product_identifier": "grow_1y_128",
-                "expires_date": "2034-04-01T06:05:44Z"
+                "product_identifier": "grow_lifetime",
+                "purchase_date": "2024-03-14T14:44:52Z"
             }
         },
-        "original_purchase_date": "2023-10-27T10:42:47Z",
-        "original_app_user_id": "$RCAnonymousID:df1e29fd84e94d0f93aff3632cbb613d",
-        "last_seen": "2024-03-27T06:01:40Z"
+        "first_seen": "2024-02-15T03:20:16Z",
+        "last_seen": "2024-03-25T21:03:17Z",
+        "management_url": "https://apps.apple.com/account/subscriptions",
+        "non_subscriptions": {
+            "grow_lifetime": [{
+                    "id": "2f58ca11d3",
+                    "is_sandbox": false,
+                    "original_purchase_date": "2024-03-14T14:44:52Z",
+                    "purchase_date": "2024-03-14T14:44:52Z",
+                    "store": "app_store",
+                    "store_transaction_id": "1090000007756035"
+                }
+            ]
+        },
+        "original_app_user_id": "$RCAnonymousID:1a71b9d1749d43a28698a77d58bcdcf4",
+        "original_application_version": "298",
+        "original_purchase_date": "2023-11-11T13:31:47Z",
+        "other_purchases": {
+            "grow_lifetime": {
+                "purchase_date": "2024-03-14T14:44:52Z"
+            }
+        },
+        "subscriptions": {
+            "grow_1y_128": {
+                "auto_resume_date": null,
+                "billing_issues_detected_at": null,
+                "expires_date": "2023-11-18T13:37:58Z",
+                "grace_period_expires_date": null,
+                "is_sandbox": false,
+                "original_purchase_date": "2023-11-11T13:37:59Z",
+                "ownership_type": "PURCHASED",
+                "period_type": "trial",
+                "purchase_date": "2023-11-11T13:37:58Z",
+                "refunded_at": null,
+                "store": "app_store",
+                "store_transaction_id": "570001360500852",
+                "unsubscribe_detected_at": "2023-11-12T16:15:30Z"
+            },
+            "grow_299_1m": {
+                "auto_resume_date": null,
+                "billing_issues_detected_at": null,
+                "expires_date": "2024-04-06T09:32:46Z",
+                "grace_period_expires_date": null,
+                "is_sandbox": false,
+                "original_purchase_date": "2024-03-03T10:32:47Z",
+                "ownership_type": "PURCHASED",
+                "period_type": "normal",
+                "purchase_date": "2024-03-06T10:32:46Z",
+                "refunded_at": null,
+                "store": "app_store",
+                "store_transaction_id": "1710000004088335",
+                "unsubscribe_detected_at": "2024-03-15T05:10:33Z"
+            }
+        }
     }
 };
 
@@ -77,5 +90,3 @@ body = JSON.stringify(obj);
 $done({
     "body": body
 });
-
-
